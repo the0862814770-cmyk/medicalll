6f87dbebfb2fefea8440354ca5bb2ee19af4137c
the0862814770-cmyk <the0862814770@gmail.com>
2026-09-01 14:06:03 +0700

    Force HTTPS scheme in production to prevent 419 CSRF token mismatch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        if (config('app.env') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
